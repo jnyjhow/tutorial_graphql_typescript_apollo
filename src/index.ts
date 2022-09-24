d1d3be52a55cd2566ccf7faccc2e9b10ec61a850
jnyjhow <jhonatan_yamane@hotmail.com>
2022-09-24 12:45:24 -0300

    update index and create new scripts in package

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"; //1
-import { context } from "./context"; 
 
 //https://studio.apollographql.com/sandbox/explorer
 import { schema } from "./schema";
+import { context } from "./context"; 
+
 export const server = new ApolloServer({
     schema,
     context,
+    introspection: true,
     //1: plugin referente ao graphql playground
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000; 
 
 server.listen({port}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
